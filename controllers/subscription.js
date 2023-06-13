59096a7d9dacfca6335774dc4d0d2165e8b7a3ea
moaaz12-web <moaaz7151@gmail.com>
2023-06-13 08:56:34 +0500

    Stripe donee :|

--- controllers/subscription.js
+++ controllers/subscription.js
@@ -4,39 +4,45 @@ const User = require("../models/user");
 
 
 module.exports = {
+  subscribed: async (req, res) => {
+    const { userid, subscription_type } = req.body;
 
-    subscribed:  async (req, res) => {
-        const { userid, subscription_type } = req.body;
-      
-        try {
-          let wordsLeft = 0;
-      
-          // Set the wordsLeft based on the subscription_type
-          if (subscription_type === "Basic") {
-            wordsLeft = 1000;
-          } else if (subscription_type === "Standard") {
-            wordsLeft = 10000;
-          } else if (subscription_type === "Premium") {
-            wordsLeft = 15000;
-          } else {
-            return res.status(400).json({ error: "Invalid subscription type" });
-          }
-          // Update the user's subscriptionStatus and wordsLeft in the database
-          const updatedUser = await User.findByIdAndUpdate(
-            userid,
-            { subscriptionStatus: subscription_type, wordsLeft },
-            { new: true }
-          );
-      
-          if (!updatedUser) {
-            return res.status(404).json({ error: "User not found" });
-          }
-      
-          return res.status(200).json({ message: "Subscription updated successfully" });
-        } catch (error) {
-          console.log(error);
-          return res.status(500).json({ error: "Internal server error" });
-        }
+    try {
+      const user = await User.findById(userid);
+
+      // Check if the user already has a subscription
+      if (user.subscriptionStatus) {
+        return res.status(200).json({ message: "User already subscribed" });
       }
 
-}
+      let wordsLeft = 0;
+
+      // Set the wordsLeft based on the subscription_type
+      if (subscription_type === "Basic") {
+        wordsLeft = 1000;
+      } else if (subscription_type === "Standard") {
+        wordsLeft = 10000;
+      } else if (subscription_type === "Premium") {
+        wordsLeft = 15000;
+      } else {
+        return res.status(400).json({ error: "Invalid subscription type" });
+      }
+
+      // Update the user's subscriptionStatus and wordsLeft in the database
+      const updatedUser = await User.findByIdAndUpdate(
+        userid,
+        { subscriptionStatus: subscription_type, wordsLeft },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      return res.status(200).json({ message: "Subscription added successfully", type: subscription_type, wordsLeft: updatedUser.wordsLeft });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  }
+};
--- server.js
+++ server.js
@@ -1,52 +1,148 @@
-const { Configuration, OpenAIApi } = require("openai");
-const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_API_KEY }));
-const cookieParser = require("cookie-parser");
-const PORT = process.env.PORT || 3000;
 const express = require("express");
+// const multer = require('multer')
+// const fs = require('fs')
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { Configuration, OpenAIApi } = require("openai");
+const openai = new OpenAIApi(
+  new Configuration({ apiKey: process.env.OPENAI_API_KEY })
+);
+
 const app = express();
-require("./config/db");
-app.use(express.json());
-app.use(cookieParser());
-require("./models/user");
+const db = require("./config/db");
+const User = require("./models/user");
 require("dotenv").config();
-app.use("/user", require("./routes/users"));
-app.use("/doc", require("./routes/document"));
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_URL}`, // Replace with your front-end URL
+    //     origin: 'http://localhost:3000', // Replace with your front-end URL
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    credentials: true,
+  })
+);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../client/public"));
-app.use("/subscription", require("./routes/subscription"));
-app.use(cors({origin: 'http://localhost:3000',methods: "GET,POST,PUT,DELETE,OPTIONS", credentials: true}));
+app.use(cookieParser());
 
+const PORT = process.env.PORT || 3000;
+app.use("/user", require("./routes/users"));
+app.use("/doc", require("./routes/document"));
+app.use("/subscription", require("./routes/subscription"));
+
+const stripe = require("stripe")(
+  `${process.env.STRIPE_SECRET_KEY}`
+);
+
+
+app.post("/create-checkout-session", async (req, res) => {
+  const { priceid, subType } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${process.env.CLIENT_URL}/users/profile?type=${subType}`,
+      // cancel_url:`${process.env.CLIENT_URL}/payments}`,
+      line_items: [{ price: priceid, quantity: 1 }],
+      mode: "subscription",
+    });
+
+    // res.status(200).json(session);
+    res.json({ url: session.url });
+  } catch (error) {
+    console.error("Error creating checkout session:", error);
+    // res.status(500).send('Error creating checkout session.');
+
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const countWords = (text) => text.trim().split(/\s+/).length;
 
 let GPT35Turbo = async (message) => {
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: message,
-    });
-    return response.data.choices[0].message.content;
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: message,
+  });
+  return response.data.choices[0].message.content;
 };
 
+app.post("/users", async (req, res) => {
+  const { userid } = req.body;
+  try {
+    const user = await User.findById(userid);
 
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const userData = {
+      wordsLeft: user.wordsLeft,
+      subscription: user.subscriptionStatus,
+      name: user.name,
+      email: user.email,
+    };
+    res.status(200).json(userData);
+  } catch (error) {
+    console.error("Error retrieving users data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
-//! GPT-3.5 Turbo AP
-app.post("/api/v1/improve", (req, res) => {
-  const { text, lang, userid } = req.body;
-  const GPT35TurboMessage = [{
-    role: "system",  
-    content: `You are a helpful assistant designed to explain complex documents to users. They will provide you with long passage of text, 
-       and you will have to simplify them and explain to them in an easy to understand language with analogies and examples (if needed).\n Explain this document in ${lang} language, easily: \n ${text}`,
-}];
-    // GPT35Turbo(GPT35TurboMessage).then((response) => {
-    //     console.log(response);
-    //     return res.send("IMPROVED TEXT");
-    //   }).catch((err) => {
-    //     console.log(err);
-    //     return res.send("ERROR");
-    //   });
+//! GPT-3.5 Turbo API
 
-    res.send("BACKEND RESP")
-  });
+app.post("/api/v1/improve", async (req, res) => {
+  try {
+    const { text, lang, userid } = req.body;
+
+    // Validate request body
+    if (!text || !lang || !userid) {
+      return res.status(400).send("Missing required fields");
+    }
+
+    // Find the user by ID
+    const user = await User.findById(userid);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    // Check if the user has words left
+    if (user.wordsLeft === 0) {
+      return res.status(403).send("No words left");
+    }
+
+    // Check if there are enough words left for the request
+    const wordsLeft = user.wordsLeft - countWords(text);
+    if (wordsLeft < 0) {
+      return res.status(403).send("Not enough words left");
+    }
+
+    // Update user's word count in the database
+    await User.findByIdAndUpdate(userid, { $set: { wordsLeft } });
+
+    return res.send("Success");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+// app.post("/api/v1/improve", (req, res) => {
+//   const { text, lang, userid } = req.body;
+//   const GPT35TurboMessage = [{
+//     role: "system",
+//     content: `You are a helpful assistant designed to explain complex documents to users. They will provide you with long passage of text,
+//        and you will have to simplify them and explain to them in an easy to understand language with analogies and examples (if needed).\n Explain this document in ${lang} language, easily: \n ${text}`,
+// }];
+//     GPT35Turbo(GPT35TurboMessage).then((response) => {
+//         console.log(response);
+//         const wordCount = countWords(reponse);
+//         return res.send("IMPROVED TEXT");
+//       }).catch((err) => {
+//         console.log(err);
+//         return res.send("ERROR");
+//       });
+//     res.send("BACKEND RESP")
+//   });
 
 app.listen(PORT, function () {
-    console.log(`Server Runs Perfectly at http://localhost:${PORT}`);
+  console.log(`Server Runs Perfectly at http://localhost:${PORT}`);
 });
